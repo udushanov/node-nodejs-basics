790a3b244db41d6aa95e58c571e296dfe535fedc
Ulugbek <u.dushanov@gmail.com>
2024-01-24 00:02:30 +0500

    feat: add solution to task cli/env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  const entries = Object.entries(process.env);
+
+  const filteredEntries = entries.filter((entry) =>
+    entry[0].startsWith("RSS_")
+  );
+
+  for (const result of filteredEntries) {
+    console.log(result[0] + "=" + result[1]);
+  }
 };
 
 parseEnv();
