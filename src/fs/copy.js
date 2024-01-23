734b3ba1ba5df0d549ae5869c083d0b179839812
Ulugbek <u.dushanov@gmail.com>
2024-01-23 17:08:17 +0500

    feat: add solution to task fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import { dirname, join } from "node:path";
+import { mkdir, readdir, copyFile } from "node:fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  const currentDir = dirname(import.meta.url).substring(8);
+  const sourcePath = join(currentDir, "files");
+  const destinationPath = join(currentDir, "files_copy");
+  
+  try {
+    const files = await readdir(sourcePath);
+    await mkdir(destinationPath, { recursive: false });
+
+    for (const file of files) {
+      const sourceFile = join(sourcePath, file);
+      const destinationFile = join(destinationPath, file);
+      await copyFile(sourceFile, destinationFile);
+    }
+
+  } catch(err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
