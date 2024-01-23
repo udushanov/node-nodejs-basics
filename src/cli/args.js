a569a1395ebb393a0f06c4d67b05e68793453ab0
Ulugbek <u.dushanov@gmail.com>
2024-01-24 00:12:45 +0500

    feat: add solution to task cli/args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,9 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  for (let i = 0; i < args.length; i += 2) {
+    console.log(args[i].substring(2) + " is " + args[i + 1] + "\n");
+  }
 };
 
 parseArgs();
