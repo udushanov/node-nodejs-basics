d9fc6474bf9aa8b67bfb3a44bf31112c1b917c6d
Ulugbek <u.dushanov@gmail.com>
2024-01-23 17:56:11 +0500

    feat: add solution to task fs/read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { dirname, join } from "node:path";
+import { readFile } from "node:fs/promises";
+
 const read = async () => {
-    // Write your code here 
+  const currentPath = dirname(import.meta.url).substring(8);
+  const filePath = join(currentPath, "files", "fileToRead.txt");
+
+  try {
+    const content = await readFile(filePath);
+    console.log("The content from the file fileToRead.txt: \n" + content);
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
