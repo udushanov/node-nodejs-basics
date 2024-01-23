77c620c0bc61a96e48bc1247209e16ee47acf4db
Ulugbek <u.dushanov@gmail.com>
2024-01-23 16:15:55 +0500

    feat: add solution to task fs/create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import { access, constants, writeFile } from "node:fs/promises";
+import {dirname, join} from 'node:path'
+
 const create = async () => {
-    // Write your code here 
+  const currentDir = dirname(import.meta.url).substring(8);
+  const filePath = join(currentDir, 'files', 'fresh.txt');
+  const message = "I am fresh and young";
+
+  try {
+    await access(filePath, constants.F_OK);
+    throw new Error("File already exists");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await writeFile(filePath, message);
+      console.log("File created and text written successfully");
+    } else {
+      throw new Error("FS operation failed");
+    }
+  }
 };
 
 await create();
