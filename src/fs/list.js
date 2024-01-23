411683d5a3a1903c846da38d85c01d93d840b68e
Ulugbek <u.dushanov@gmail.com>
2024-01-23 17:46:58 +0500

    feat: add solution to task fs/list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import { dirname, join } from "node:path";
+import { readdir } from "node:fs/promises";
+
 const list = async () => {
-    // Write your code here 
+  const currentPath = dirname(import.meta.url).substring(8);
+  const filePath = join(currentPath, "files");
+
+  try {
+    const fileNames = await readdir(filePath);
+    console.log('An array of filenames from files directory:')
+    console.log(fileNames)
+  } catch (err) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
