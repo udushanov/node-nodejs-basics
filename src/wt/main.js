9f5dba07e67ef62c9794d0b3bc8305353ef5bccd
Ulugbek <u.dushanov@gmail.com>
2024-01-24 21:34:33 +0500

    feat: add solution to task wt/worker.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,24 @@
+import { Worker, isMainThread } from "node:worker_threads";
+import { dirname, join } from "node:path";
+
 const performCalculations = async () => {
-    // Write your code here
+  const currentDir = dirname(import.meta.url).substring(8);
+  const workerPath = join(currentDir, "worker.js");
+
+  if (isMainThread) {
+    const n = 10;
+
+    const worker = new Worker(workerPath, {
+      workerData: n,
+    });
+
+    return new Promise((resolve) => {
+      worker.on("message", (result) => {
+        console.log(`The result of nthFibonacci(${n}) is: ${result}`);
+        resolve(result);
+      });
+    });
+  }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,17 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { parentPort, workerData, isMainThread } from "node:worker_threads";
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+const sendResult = (result) => {
+  if (isMainThread) {
+    throw new Error("sendResult should be called inside the worker thread");
+  }
+
+  parentPort.postMessage(result);
 };
 
-sendResult();
+if (!isMainThread) {
+  const n = workerData;
+  const result = nthFibonacci(n);
+  sendResult(result);
+}
