eef5532b502f57dbe75f543278ea1a1f16edfbfc
Ulugbek <u.dushanov@gmail.com>
2024-01-24 11:42:33 +0500

    feat: add solution to task streams/transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { Transform } from "node:stream";
+
+const reverseTransform = () => {
+  return new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedText = chunk.toString().split("").reverse().join("");
+      this.push(reversedText);
+      callback();
+    },
+  });
+};
+
 const transform = async () => {
-    // Write your code here 
+  const reverseStream = reverseTransform();
+
+  process.stdin.pipe(reverseStream).pipe(process.stdout);
 };
 
 await transform();
