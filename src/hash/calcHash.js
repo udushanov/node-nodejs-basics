f88742c42abc99d3124983575fcee7e902752356
Ulugbek <u.dushanov@gmail.com>
2024-01-24 11:02:41 +0500

    feat: add soltuion to task hash/calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { dirname, join } from "node:path";
+import { readFile } from "node:fs/promises";
+const { createHmac } = await import("node:crypto");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const currentDir = dirname(import.meta.url).substring(8);
+  const filePath = join(currentDir, "files", "fileToCalculateHashFor.txt");
+  const buffer = await readFile(filePath);
+  const text = buffer.toString();
+
+  const hmac = createHmac('sha256', 'secret');
+
+  hmac.on('readable', () => {
+    const data = hmac.read();
+    
+    if (data) {
+      console.log(data.toString('hex'));
+    }
+  })
+  
+  hmac.write(text);
+  hmac.end();
 };
 
 await calculateHash();
