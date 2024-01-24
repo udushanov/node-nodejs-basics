ecdaf598df00e63f3f62d9905026772944563aff
Ulugbek <u.dushanov@gmail.com>
2024-01-24 21:45:03 +0500

    feat: add solution to task wt/main.js

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,23 +1,47 @@
 import { Worker, isMainThread } from "node:worker_threads";
 import { dirname, join } from "node:path";
+import { cpus } from "node:os";
 
 const performCalculations = async () => {
   const currentDir = dirname(import.meta.url).substring(8);
   const workerPath = join(currentDir, "worker.js");
+  const NUM_CORES = cpus().length;
 
   if (isMainThread) {
-    const n = 10;
+    const workers = [];
+    const results = [];
 
-    const worker = new Worker(workerPath, {
-      workerData: n,
-    });
-
-    return new Promise((resolve) => {
-      worker.on("message", (result) => {
-        console.log(`The result of nthFibonacci(${n}) is: ${result}`);
-        resolve(result);
+    for (let i = 0; i < NUM_CORES; i++) {
+      const worker = new Worker(workerPath, {
+        workerData: i + 10,
       });
-    });
+
+      workers.push(worker);
+
+      worker.on("message", (result) => {
+        results.push({
+          status: "resolved",
+          data: result,
+        });
+
+        if (results.length === NUM_CORES) {
+          console.log("Results:", results);
+        }
+      });
+
+      worker.on("error", (error) => {
+        results.push({
+          status: "error",
+          data: null,
+        });
+
+        if (results.length === NUM_CORES) {
+          console.log("Results:", results);
+        }
+
+        console.error(`Worker error: ${error}`);
+      });
+    }
   }
 };
 
